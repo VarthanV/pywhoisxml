61f6caff2138eb49fcddbcc5284f17f29f856d8f
Vishnu Varthan <vichu@Vishnus-MacBook-Air.local>
2020-04-29 22:34:01 +0530

    Added Domain Reputation and Geo Location Modules

---
+++ pywhoisxml/domain_reputation.py
@@ -0,0 +1,23 @@
+from pywhoisxml.conf import URL_DEFAULTS, get_response, return_value
+from pywhoisxml.exceptions import PyWhoisException
+
+
+class DomainReputation(object):
+    def __init__(self, api_key, domain, output_format="JSON", **kwargs):
+        self.api_key = api_key
+        self.domain = domain
+        self.url = URL_DEFAULTS.get('reputation')
+        self.params = {
+            "apiKey": api_key,
+            "outputFormat": output_format,
+            "domainName": domain
+
+        }
+        self.params.update(kwargs)
+        self.response = get_response(self.url, self.params)
+    @property
+    def data(self):
+         return self.response   
+    @property
+    def score(self):
+        return return_value(self.response,"reputationScore")
---
+++ pywhoisxml/ip_geo.py
@@ -0,0 +1,39 @@
+from pywhoisxml.conf import URL_DEFAULTS, get_response,return_value
+from pywhoisxml.exceptions import PyWhoisException
+
+class IpGeo(object):
+    def __init__(self,api_key,ip_address,output_format = "JSON",**kwargs):
+        self.api_key = api_key
+        self.ip_address  = ip_address
+        self.url=URL_DEFAULTS.get('ip_address')
+        self.params = {
+            "apiKey":api_key,
+            "outputFormat":output_format,
+            "ipAddress" :self.ip_address
+
+        }
+        self.params.update(kwargs)
+        self.response =get_response(self.url,self.params)
+        self.filtered_response = self.response['location']
+     
+    @property
+    def country(self):
+        return return_value(self.filtered_response,'country')  
+    @property
+    def region(self):
+        return return_value(self.filtered_response,'region')
+    @property
+    def city(self):
+        return return_value(self.filtered_response,'city')
+    @property
+    def latitude(self):
+        return return_value(self.filtered_response,'lat')
+    @property
+    def longitude(self):
+        return return_value(self.filtered_response,"long")
+    @property
+    def postal_code(self):
+        return return_value(self.filtered_response,"postalCode")
+    @property
+    def time_zone(self):
+        return return_value(self.filtered_response,"timezone")                    
