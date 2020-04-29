bae84af9669b7f6897c43146b784f6b6c469603f
Vishnu Varthan <vichu@Vishnus-MacBook-Air.local>
2020-04-29 22:33:08 +0530

    Cleanup in Function

--- pywhoisxml/email_verification.py
+++ pywhoisxml/email_verification.py
@@ -1,37 +1,35 @@
-from pywhoisxml.conf import URL_DEFAULTS, get_response
+from pywhoisxml.conf import URL_DEFAULTS, get_response,return_value
 from pywhoisxml.exceptions import PyWhoisException
 
 
 class EmailVerification(object):
-    def __init__(self, api_key, email):
+    def __init__(self, api_key, email,output_format="JSON",**kwargs):
         self.api_key =api_key
         self.url = URL_DEFAULTS.get("email_verification")
         self.params = {
             "apiKey": self.api_key,
-            "outputFormat": "JSON",
+            "outputFormat": output_format,
             "emailAddress": email
         }
+        self.params.update(kwargs)
         self.response = get_response(self.url, self.params)
 
     @property
     def data(self):
         return self.response
 
-    def return_value(self, key):
-        return bool(self.response.get(key))
-
     @property
     def format_check(self):
-        return self.return_value('formatCheck')
+        return bool(return_value(self.response,'formatCheck'))
 
     @property
     def smtp_check(self):
-        return self.return_value('smtpCheck')
+        return bool(return_value(self.response,'smtpCheck'))
 
     @property
     def dns_check(self):
-        return self.return_value('dnsCheck')
+        return bool(return_value(self.response,'dnsCheck'))
 
     @property
     def catch_all_checks(self):
-        return self.return_value('catchAllCheck')
+        return bool(return_value(self.response,'catchAllCheck'))
