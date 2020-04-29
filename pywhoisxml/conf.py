c3e205c4329c5651784aea2ea9cfa7b7770dcb22
Vishnu Varthan <vichu@Vishnus-MacBook-Air.local>
2020-04-29 22:32:53 +0530

    Updated URLS

--- pywhoisxml/conf.py
+++ pywhoisxml/conf.py
@@ -2,7 +2,10 @@ import requests
 from pywhoisxml.exceptions import PyWhoisException
 URL_DEFAULTS = {
     "lookup_url": "https://www.whoisxmlapi.com/whoisserver/WhoisService",
-    "email_verification":"https://emailverification.whoisxmlapi.com/api/v1"
+    "email_verification":"https://emailverification.whoisxmlapi.com/api/v1",
+    "screenshot":"https://website-screenshot.whoisxmlapi.com/api/v1",
+    "ip_address":"https://ip-geolocation.whoisxmlapi.com/api/v1",
+    "reputation":"https://domain-reputation.whoisxmlapi.com/api/v1"
 }
 
 
@@ -14,5 +17,7 @@ def get_response(url, params):
         return response
     else :    
       raise PyWhoisException(
-        "Make sure you have passed the right params ,Please Try again later")
+        "Make sure you have passed the right params ,Please Try again later",response)
   
+def return_value(response,key):
+        return response.get(key)
